7140ed5f527712f3903064b9396740e4bb6b1e7b
binhan <tranbinhan223@gmail.com>
2024-03-14 10:13:37 +0700

    3rd part completed

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -21,6 +21,6 @@ const messageSchema = new mongoose.Schema(
     {timestamps: true}
 );
 
-const Message = mongoose.model('Message', messageSchema)
+const Message = mongoose.model('Messages', messageSchema)
 
 export default Message
